8c086b2acdfc1cd795be019d5139c1e5c36392d0
just-a-guy150 <dimonmig301@gmail.com>
2025-05-03 11:08:28 +0300

    added package to project

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+
+
+});
+
+
+server.listen(3000);
+

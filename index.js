9a859d527139f7f5dd0e00a91460cde41278318a
just-a-guy150 <dimonmig301@gmail.com>
2025-05-17 11:32:57 +0300

    added getting jokes by id

--- index.js
+++ index.js
@@ -5,49 +5,111 @@ const fs = require('fs');
 const dataPath = path.join(__dirname, 'data');
 
 const server = http.createServer((request, response) => {
-    if( request.url === "/jokes") {
-        switch (request.method) {
-            case 'GET':
-                getAllJokes( response );
-                break;
-            case 'POST':
-                addJoke( request );
-                break;
-        }
-    }
+    let url = request.url;
+    let param
+    if (request.url.includes('?')) {
+        [url, param] = request.url.split('?')
+        var id = param.split('=')[1];
+    };
 
+
+
+    switch (url) {
+        case '/jokes':
+            if (request.method === 'GET') getAllJokes(response);
+            if (request.method === 'POST') addJoke(request, response);
+            break;
+        case '/like':
+            setLike(response, id);
+            break;
+        case '/joke':
+            getJoke(response, id);
+            break;
+        case '/dislike':
+            setDislike(response, id);
+            break;
+        default:
+            sendNotFound(response);
+            break;
+    }
 });
 
-function getAllJokes( response ) {
-    const jokes = [];
+function getJoke(response, id) {
+    const jokePath = path.join(dataPath, `${id}.json`);
+    const jokeContent = fs.readFileSync(jokePath, 'utf-8');
+    const jokeObject = JSON.parse(jokeContent);
 
-    const jokeFileNames = fs.readdirSync( dataPath );
-    
-    for (const fileName of jokeFileNames) {
-        const filePath = path.join( dataPath, fileName );
+    const joke = [jokeObject.content];
 
-        const fileContent = fs.readFileSync( filePath, 'utf-8' );
-        const fileObject = JSON.parse( fileContent );
-        
-        jokes.push( fileObject.content );
-    }
 
-    response.writeHead(200, {"Content-type": 'application/json'});
-    response.end( JSON.stringify( jokes ) );
+    response.writeHead(200, { "Content-type": 'application/json' });
+    response.end(JSON.stringify(joke));
 }
 
-function addJoke ( request ) {
+function getAllJokes(response) {
+    const jokes = [];
+
+    const jokeFileNames = fs.readdirSync(dataPath);
+
+    for (const fileName of jokeFileNames) {
+        const filePath = path.join(dataPath, fileName);
+
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const fileObject = JSON.parse(fileContent);
+
+        jokes.push(fileObject.content);
+    }
+
+    response.writeHead(200, { "Content-type": 'application/json' });
+    response.end(JSON.stringify(jokes));
+}
+
+function addJoke(request, response) {
     let data = '';
-    request.on("data", function(chunk) {
+    request.on("data", function (chunk) {
         data += chunk;
     });
-    request.on("end", function() {
-        const newJoke = JSON.parse( data );
-        const fileName = Date.now();
-        const filePath = path.join( dataPath, `${fileName}.json` );
-        fs.writeFileSync( filePath, JSON.stringify( newJoke ) );
-        console.log( data );
+    request.on("end", function () {
+        const joke = JSON.parse(data);
+        joke.likes = 0;
+        joke.dislikes = 0;
+        const fileName = fs.readdirSync(dataPath).length + ".json";
+        const filePath = path.join(dataPath, fileName);
+        fs.writeFileSync(filePath, JSON.stringify(joke));
+        response.writeHead(200, { "Content-type": 'application/json' });
+        response.end(JSON.stringify(joke));
     })
+}
+
+function setLike(response, id) {
+
+    const jokePath = path.join(dataPath, `${id}.json`);
+    const jokeContent = fs.readFileSync(jokePath, 'utf-8');
+    const jokeObject = JSON.parse(jokeContent);
+
+    jokeObject.likes++;
+    fs.writeFileSync(jokePath, JSON.stringify(jokeObject));
+
+    response.writeHead(200);
+    response.end("like has been added");
+}
+
+function setDislike(response, id) {
+
+    const jokePath = path.join(dataPath, `${id}.json`);
+    const jokeContent = fs.readFileSync(jokePath, 'utf-8');
+    const jokeObject = JSON.parse(jokeContent);
+
+    jokeObject.dislikes++;
+    fs.writeFileSync(jokePath, JSON.stringify(jokeObject));
+
+    response.writeHead(200);
+    response.end("dislike has been added");
+}
+
+function sendNotFound(response) {
+    response.writeHead(404, { "Content-type": 'text/plain' });
+    response.end("Not Found");
 }
 
 server.listen(3000);
